5252db7c199b5e8b344ed16f15a731bbd6976b65
Wang Yumeng <wangyumeng02@megvii.com>
2023-03-16 08:36:28 +0800

    ✨ feat(train.sh): Added train.sh
    Added bash script `train.sh` for quick start of the training and sampling program.

---
+++ train.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+export PYTHONPATH=$(pwd)
+export NCCL_P2P_DISABLE=1
+
+mpiexec -n 8 \
+  python3 scripts/image_train.py \
+    --data_dir=/data/bzj_trash/images/green \
+    --log_interval=100 \
+    --image_size=512 \
+    --batch_size=1 \
+    --save_interval=10000 \
+    --log_dir=log/train # --resume_checkpoint=log/train/model030000.pt
+
+python3 scripts/image_sample.py \
+  --num_samples=1 \
+  --image_size=512 \
+  --batch_size=1 \
+  --model_path=log/train/model020000.pt \
+  --log_dir=log/sample
